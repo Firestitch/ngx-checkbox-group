0576f07a035aafbfe9da832f628ef748c8c5e2d5
Raymond Gigliotti <ray@firestitch.com>
2019-07-16 16:11:04 -0400

    listened to mat-checkbox content changes

--- playground/app/components/horizontal/horizontal.component.ts
+++ playground/app/components/horizontal/horizontal.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'horizontal',
   templateUrl: 'horizontal.component.html'
 })
-export class HorizontalComponent {
+export class HorizontalComponent implements OnInit {
 
   selected: object[] = [];
 
-  items = [
-      { name: 'Item 1', id: 1 },
-      { name: 'Item 2', id: 2 },
-      { name: 'Item 3', id: 3 },
-      { name: 'Item 4', id: 4 }
-    ];
+  items = [];
+
+  ngOnInit() {
+
+    // Simulate API deplay
+    setTimeout(() => {
+        this.items = [
+        { name: 'Item 1', id: 1 },
+        { name: 'Item 2', id: 2 },
+        { name: 'Item 3', id: 3 },
+        { name: 'Item 4', id: 4 }
+      ];
+    }, 50);
+  }
 }
--- src/app/components/checkbox-group/checkbox-group.component.ts
+++ src/app/components/checkbox-group/checkbox-group.component.ts
@@ -9,12 +9,13 @@ import {
   DoCheck,
   AfterContentInit,
   OnDestroy,
-  Provider,
   forwardRef,
   HostBinding
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatCheckbox } from '@angular/material';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 
 @Component({
@@ -44,73 +45,97 @@ export class FsCheckboxGroupComponent implements AfterContentInit, DoCheck, OnDe
   public contentChildren: QueryList<MatCheckbox>;
 
   private innerValue: any[];
-
-  public iterableDiffer;
+  private _destroy$ = new Subject();
+  private _differChildren;
+  private _differValue;
 
   constructor(private iterableDiffers: IterableDiffers) {
-    this.iterableDiffer = this.iterableDiffers.find([]).create(null);
+    this._differChildren = this.iterableDiffers.find([]).create(null);
+    this._differValue = this.iterableDiffers.find([]).create(null);
   };
 
- private onTouchedCallback: () => void = () => { };
- private onChangeCallback: (_: any) => void = () => { };
+  private onTouchedCallback: () => void = () => { };
+  private onChangeCallback: (_: any) => void = () => { };
 
- ngDoCheck() {
-   const changes = this.iterableDiffer.diff(this.innerValue);
-   if (changes && this.innerValue) {
-     this.contentChildren.toArray().forEach((input) => {
-       const index = this.innerValue.indexOf(input.value);
-       if (index !== -1) {
-         input.checked = true;
-       } else {
-         input.checked = false;
-       }
-     });
+  ngDoCheck() {
+    const changes = this._differValue.diff(this.innerValue);
+    if (changes && this.innerValue) {
 
-     this.onChangeCallback(this.innerValue);
-     this.change.emit(this.innerValue);
-   }
- }
+      this.contentChildren.toArray().forEach((input) => {
+        const index = this.innerValue.indexOf(input.value);
+        if (index !== -1) {
+          input.checked = true;
+        } else {
+          input.checked = false;
+        }
+      });
 
- public ngAfterContentInit() {
-   this.contentChildren.toArray().forEach((input) => {
-     input.change
-       .subscribe((value) => {
-         if (value.checked) {
-           this.innerValue.push(value.source.value);
-         } else {
-           const index = this.innerValue.indexOf(input.value);
-           if (index !== -1) {
-             this.innerValue.splice(index, 1);
-           }
-         }
-       });
-   });
- }
+      this.onChangeCallback(this.innerValue);
+      this.change.emit(this.innerValue);
+    }
+  }
 
- public isEquals(obj1, obj2): boolean {
-   return JSON.stringify(obj1) === JSON.stringify(obj2);
- }
+  public ngAfterContentInit() {
 
- public writeValue(value: any) {
+    let changeDiff = this._differChildren.diff(this.contentChildren);
+    if (changeDiff) {
+      changeDiff.forEachAddedItem(change => {
+        this._addCheckbox(change.item);
+      });
+    }
 
-   if (!this.isEquals(value, this.innerValue)) {
-       this.innerValue = value;
-   }
- }
+    this.contentChildren.changes
+      .pipe(
+        takeUntil(this._destroy$),
+      )
+      .subscribe(fsModelObjects => {
+        changeDiff = this._differChildren.diff(fsModelObjects);
+        if (changeDiff) {
+          changeDiff.forEachAddedItem((change) => {
+            this._addCheckbox(change.item);
+          });
+        }
+      });
+  }
 
- public registerOnChange(fn: any) {
-   this.onChangeCallback = fn;
- }
+  private _addCheckbox(input) {
+    input.change
+    .pipe(
+      takeUntil(this._destroy$),
+    )
+    .subscribe((value) => {
+      if (value.checked) {
+        this.innerValue.push(value.source.value);
+      } else {
+        const index = this.innerValue.indexOf(input.value);
+        if (index !== -1) {
+          this.innerValue.splice(index, 1);
+        }
+      }
+    });
+  }
 
- public registerOnTouched(fn: any) {
-    this.onTouchedCallback = fn;
- }
+  public isEquals(obj1, obj2): boolean {
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
+  }
 
- public ngOnDestroy() {
-   this.contentChildren.toArray().forEach((input) => {
-     input.change.unsubscribe();
-   });
-   this.change.unsubscribe();
- }
+  public writeValue(value: any) {
 
+    if (!this.isEquals(value, this.innerValue)) {
+        this.innerValue = value;
+    }
+  }
+
+  public registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  public registerOnTouched(fn: any) {
+      this.onTouchedCallback = fn;
+  }
+
+  public ngOnDestroy() {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
 }
