994ef3641b01fdf5bdfb2bd178021ffa5ff23b71
ArioShaman <ariosha6@gmail.com>
2023-03-09 14:54:43 +0300

    FC-T472_Strict_templates

--- playground/app/components/horizontal/horizontal.component.ts
+++ playground/app/components/horizontal/horizontal.component.ts
@@ -7,26 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorizontalComponent implements OnInit {
 
-  public selected: any[] = [
-    { name: 'Item 1', id: 1 },
-  ];
+  public selected: any[] = [];
 
   public items = [];
 
-  ngOnInit() {
-
+  public ngOnInit() {
     setTimeout(() => {
-        this.items = [
+      this.items = [
         { name: 'Item 1', id: 1, disabled: true },
         { name: 'Item 2', id: 2 },
         { name: 'Item 3', id: 3 },
         { name: 'Item 4', id: 4 }
       ];
+
+      this.selected = [this.items[0]];
     }, 50);
   }
 
   public compareWith(o1, o2) {
     return o1 && o2 && o1.id === o2.id;
   }
-
 }
--- playground/app/components/vertical/vertical.component.ts
+++ playground/app/components/vertical/vertical.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'vertical',
   templateUrl: 'vertical.component.html'
 })
-export class VerticalComponent {
+export class VerticalComponent implements OnInit {
 
-  selected: any[] = [];
+  public selected: any[] = [];
 
-  items = [
-      { name: 'Item 1', id: 1 },
-      { name: 'Item 2', id: 2 },
-      { name: 'Item 3', id: 3 },
-      { name: 'Item 4', id: 4 }
-    ];
+  public items = [];
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.items = [
+        { name: 'Item 1', id: 1, disabled: true },
+        { name: 'Item 2', id: 2 },
+        { name: 'Item 3', id: 3 },
+        { name: 'Item 4', id: 4 }
+      ];
+
+      this.selected = [this.items[0]];
+    }, 50);
+  }
 }
